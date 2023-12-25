aa2be0157d5ed92bd8178ed1c30baefbc83a2b87
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-25 22:07:29 +0900

    tired

--- preload.js
+++ preload.js
@@ -1,11 +1,7 @@
-window.addEventListener('DOMContentLoaded', () => {
-	const replaceText = (selector, text) => {
-		const element = document.getElementById(selector)
-		if (element) element.innerText = text
-	}
+const { contextBridge } = require('electron/renderer')
 
-	for (const dependency of ['chrome', 'node', 'electron']) {
-		replaceText(`${dependency}-version`, process.versions[dependency])
-	}
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
 })
-
