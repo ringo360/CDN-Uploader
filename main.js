174d49ba47fd80d7ee44ec14100f853a6e7b2365
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-25 21:37:59 +0900

    hello world

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('node:path')
 
+console.log('Loading...')
 
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 800,
-		height: 600
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
 	})
-
 	win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-	createWindow();
+	console.log('App is ready!')
+	createWindow()
+
+	app.on('activate', () => {
+		console.log('on activate')
+		if (BrowserWindow.getAllWindows.length === 0) createWindow()
+	})
 })
+
+app.on('window-all-closed', () => {
+	console.log('detect window-all-closed')
+	if (process.platform !== 'darwin') {
+		console.log('quit')
+		app.quit()
+	}
+})
+
